b7302f19d10edf61c09f239edd3ff30c8bf10e80
Guhsjdkmsinaunainainsunhananajenau897 <guddupandit84000@gmail.com>
2024-12-04 18:08:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,10 +35,10 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
-LOGGER_ID = int(getenv("LOGGER_ID", -1002134214481))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002067001953)
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-OWNER_ID = int(getenv("OWNER_ID", 7463345743))
+OWNER_ID = int(getenv("OWNER_ID", 6283192619))
 # -----------------------------------------------------------------
 # -----------------------------------------------------------------
 
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/istkharalam62/THUNDER-",
+    "https://github.com/Guhsjdkmsinaunainainsunhananajenau897/land-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
